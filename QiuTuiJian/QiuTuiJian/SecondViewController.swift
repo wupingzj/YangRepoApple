91908a86a0a96913014514c67e3ee2f3ae253df0
Kevin Wu <kevinpingwu2@gmail.com>
2014-07-13 10:32:00 +1000

    Initial QiuTuiJian tabbed template

---
+++ QiuTuiJian/QiuTuiJian/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QiuTuiJian
+//
+//  Created by Ping on 13/07/2014.
+//  Copyright (c) 2014 Yang Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
