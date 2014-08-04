bdfe99a73f89fa581fb510938557aa3374f7bec0
Kevin Wu <kevinpingwu2@gmail.com>
2014-08-04 22:17:01 +1000

    As data was changed without being versioned, use new data store QiuTuiJianV0d.sqlite instead to let tests pass.

--- QiuTuiJian/QiuTuiJian/DataService.swift
+++ QiuTuiJian/QiuTuiJian/DataService.swift
@@ -63,7 +63,7 @@ public class DataService {
     // If the coordinator doesn't already exist, it is created and the application's store added to it.
     public var sharedPSC: NSPersistentStoreCoordinator {
         if !_persistentStoreCoordinator {
-            let storeURL = self.applicationDocumentsDirectory.URLByAppendingPathComponent("QiuTuiJianV0c.sqlite")
+            let storeURL = self.applicationDocumentsDirectory.URLByAppendingPathComponent("QiuTuiJianV0d.sqlite")
             println(storeURL)
             
             
