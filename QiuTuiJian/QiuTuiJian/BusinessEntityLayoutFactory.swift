e85bd99d5d71e85e65867d919800331577f473e6
Kevin Wu <kevinpingwu2@gmail.com>
2014-09-10 17:17:48 +1000

    Please choose a BusinessEntity is working appropriately now. Great!

--- QiuTuiJian/QiuTuiJian/BusinessEntityLayoutFactory.swift
+++ QiuTuiJian/QiuTuiJian/BusinessEntityLayoutFactory.swift
@@ -25,45 +25,24 @@ class BusinessEntityLayoutFactory {
     
     public func showBusinessEntityMissingMessage() {
         // errorMsgLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
-        let screenSize: CGSize = UIScreen.mainScreen().bounds.size
-        
-        let msg: String = "Please go back to select a business entity."
-        let errorMsg: UITextView = UITextView(frame: CGRectMake(leading, top_space, screenSize.width - leading - trailing, 200))
-        
-        errorMsg.editable = false
-        errorMsg.selectable = false
-        errorMsg.font = UIFont(name: nil, size: 20)
-//        let cgRect = CGRectMake(contentView.frame.origin.x, contentView.frame.origin.y, errorMsg.contentSize.width, errorMsg.contentSize.height);
+        let errorMsg: UITextView = UITextView()
+        var viewsDictionary: Dictionary = ["contentView":contentView, "errorMsg":errorMsg]
 
-        errorMsg.text = msg
+        //let screenSize: CGSize = UIScreen.mainScreen().bounds.size
+        //let cgRect = CGRectMake(contentView.frame.origin.x, contentView.frame.origin.y, errorMsg.contentSize.width, errorMsg.contentSize.height);
+
+        errorMsg.font = UIFont(name: nil, size: 20)
+        errorMsg.text = "Please go back and select a business entity."
+        // This line must be before the constraints as constraints needs to know the superview
         contentView.addSubview(errorMsg)
         
-// The following works but doesn't have impact on GUI.
-//        The constrains are not in effect and so greyed out
-        
-//        let sizeLabel: UILabel = UILabel(frame: CGRectMake(leading, 8*top_space, 600, 20))
-        let sizeLabel: UILabel = UILabel()
-
-//        sizeLabel.text = "width \(self.contentView.frame.width), height=\(self.contentView.frame.height)"
-        sizeLabel.text = "width \(errorMsg.contentSize.width), height=\(errorMsg.contentSize.height) abcd cefkljlk f ljdfladjsf  aksdfajdfa;sfdhk; hadsfha;sfha;dfa;dsnf;a jfdasdfnkasnfaksdjn kadfskjfdakjf7777777"
-        sizeLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
-        contentView.addSubview(sizeLabel)
-        
-
-        var viewsDictionary: Dictionary = ["sizeLabel":sizeLabel, "contentView":contentView, "errorMsg":errorMsg]
-        var constraintsH = NSLayoutConstraint.constraintsWithVisualFormat("H:|-50-[sizeLabel]-50-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
-//        var constraintsV = NSLayoutConstraint.constraintsWithVisualFormat("V:|-20-[sizeLabel]-20-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
-        var constraintsV = NSLayoutConstraint.constraintsWithVisualFormat("V:|-120-[sizeLabel]", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
-        
-        
-        var constraintsSizeH = NSLayoutConstraint.constraintsWithVisualFormat("H:[sizeLabel(>=50)]", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
-        var constraintsSizeV = NSLayoutConstraint.constraintsWithVisualFormat("V:[sizeLabel(20)]", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
+        // errorMsg location
+        errorMsg.setTranslatesAutoresizingMaskIntoConstraints(false)
+        var constraintsH = NSLayoutConstraint.constraintsWithVisualFormat("H:|-[errorMsg(>=50)]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
+        var constraintsV = NSLayoutConstraint.constraintsWithVisualFormat("V:|-[errorMsg(100)]", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
         
         self.contentView.addConstraints(constraintsH)
         self.contentView.addConstraints(constraintsV)
-        
-        sizeLabel.addConstraints(constraintsSizeV)
-        sizeLabel.addConstraints(constraintsSizeH)
     }
     
     func getContentView() -> UIView {
